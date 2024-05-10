8c0eeddd67c42b18acae5fbc753832bcc422a9a9
Martin Adam <martin.viana99@gmail.com>
2024-05-10 06:31:34 -0300

    Estructural Changes + Mobile Drawer

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -43,17 +43,17 @@ export default function Header({
   },
 }: Nav) {
   return (
-    <nav class="drawer drawer-end sticky top-0 w-full bg-pink-600 py-5 z-50 backdrop-blur-lg border-b-4 border-yellow-500">
+    <nav class="drawer drawer-end fixed z-50 bg-pink-600 py-5 lg:px-10 border-b-4 border-yellow-500">
       <input id="mobile-drawer-nav" type="checkbox" class="drawer-toggle" />
 
       {/* main content */}
-      <div class="drawer-content container lg:px-0 px-4 flex gap-8 items-center justify-between py-4">
+      <div class="drawer-content container lg:px-0 px-4 flex gap-8 items-center justify-between">
         <a href="/">
           <Image src={logo.src || ""} width={100} height={28} alt={logo.alt} />
         </a>
 
         <div class="hidden items-center justify-between lg:flex w-full">
-          <ul class="flex">
+          <ul class="flex flex-col lg:flex-row gap-2 w-full lg:items-center lg:justify-between text-white text-sm font-semibold">
             {navigation.links.map((link) => (
               <li>
                 <a
@@ -73,7 +73,7 @@ export default function Header({
                 id={item?.id}
                 href={item?.href ?? "#"}
                 target={item?.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary ${
+                class={`font-normal btn btn-primary rounded-xl p-2 bg-purple-700 text-white font-semibold hover:bg-purple-800 duration-300 ease-in-out transition-all ${
                   item.outline && "btn-outline"
                 }`}
               >
@@ -85,7 +85,7 @@ export default function Header({
 
         <label
           htmlFor="mobile-drawer-nav"
-          class="flex lg:hidden btn btn-ghost drawer-button"
+          class="flex lg:hidden btn btn-ghost drawer-button text-white"
         >
           <Icon id="Bars3" size={24} strokeWidth={0.1} />
         </label>
@@ -100,7 +100,7 @@ export default function Header({
           class="drawer-overlay"
         />
 
-        <div class="flex flex-col gap-8 min-h-full w-80 bg-base-100 text-base-content">
+        <div class="flex flex-col gap-8 min-h-full w-80 bg-pink-600 text-base-content ">
           <a class="p-4" href="/">
             <Image
               src={logo.src || ""}
@@ -110,7 +110,7 @@ export default function Header({
             />
           </a>
 
-          <ul class="menu">
+          <ul class="menu gap-2 w-full lg:items-center lg:justify-between text-white text-sm font-semibold">
             {navigation?.links.map((link) => (
               <li>
                 <a href={link.url} aria-label={link.label}>
@@ -127,7 +127,7 @@ export default function Header({
                 id={item?.id}
                 href={item?.href ?? "#"}
                 target={item?.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary ${
+                class={`font-normal btn btn-primary rounded-xl p-2 bg-purple-700 text-white font-semibold hover:bg-purple-800 duration-300 ease-in-out transition-all ${
                   item.outline && "btn-outline"
                 }`}
               >
