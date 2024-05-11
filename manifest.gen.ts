686bf62da84113acfcac066698b0074e08e2e203
jovenan <jovenan2001@gmail.com>
2024-05-11 19:01:28 -0300

    feat: update manifest

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -10,15 +10,16 @@ import * as $$$$$$2 from "./sections/BlogPosts.tsx";
 import * as $$$$$$3 from "./sections/Faq.tsx";
 import * as $$$$$$4 from "./sections/Footer.tsx";
 import * as $$$$$$5 from "./sections/Header.tsx";
-import * as $$$$$$12 from "./sections/hero-1.tsx";
-import * as $$$$$$13 from "./sections/hero-2.tsx";
+import * as $$$$$$13 from "./sections/hero-1.tsx";
+import * as $$$$$$14 from "./sections/hero-2.tsx";
 import * as $$$$$$6 from "./sections/Hero.tsx";
-import * as $$$$$$14 from "./sections/hero2.tsx";
+import * as $$$$$$15 from "./sections/hero2.tsx";
 import * as $$$$$$7 from "./sections/ImageWithParagraph.tsx";
 import * as $$$$$$8 from "./sections/ImpactAndTestimonials.tsx";
 import * as $$$$$$9 from "./sections/Logos.tsx";
 import * as $$$$$$10 from "./sections/Testimonials.tsx";
 import * as $$$$$$11 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$12 from "./sections/WhoCanParticipate.tsx";
 
 const manifest = {
   "sections": {
@@ -28,15 +29,16 @@ const manifest = {
     "site/sections/Faq.tsx": $$$$$$3,
     "site/sections/Footer.tsx": $$$$$$4,
     "site/sections/Header.tsx": $$$$$$5,
-    "site/sections/hero-1.tsx": $$$$$$12,
-    "site/sections/hero-2.tsx": $$$$$$13,
+    "site/sections/hero-1.tsx": $$$$$$13,
+    "site/sections/hero-2.tsx": $$$$$$14,
     "site/sections/Hero.tsx": $$$$$$6,
-    "site/sections/hero2.tsx": $$$$$$14,
+    "site/sections/hero2.tsx": $$$$$$15,
     "site/sections/ImageWithParagraph.tsx": $$$$$$7,
     "site/sections/ImpactAndTestimonials.tsx": $$$$$$8,
     "site/sections/Logos.tsx": $$$$$$9,
     "site/sections/Testimonials.tsx": $$$$$$10,
     "site/sections/Theme/Theme.tsx": $$$$$$11,
+    "site/sections/WhoCanParticipate.tsx": $$$$$$12,
   },
   "apps": {
     "site/apps/decohub.ts": $$$$$$$$$$$0,
