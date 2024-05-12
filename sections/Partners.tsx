0e5db8eb480c7cd5b319971c29395c2def2327c6
Martin Adam <martin.viana99@gmail.com>
2024-05-12 03:27:08 -0300

    add Partners section

---
+++ sections/Partners.tsx
@@ -0,0 +1,87 @@
+import Image from "apps/website/components/Image.tsx";
+import type { ImageWidget } from "apps/admin/widgets.ts";
+
+interface Partners {
+  logo?: ImageWidget;
+  alt?: string;
+}
+
+interface Props {
+  /**
+   * @description Section Title.
+   */
+  title?: string;
+  /**
+   * @description Display Partners Logos.
+   * @title {{{alt}}}
+   */
+  partners?: Partners[];
+}
+
+export default function Section(
+  {
+    title = "Quem faz a Maratona Tech",
+    partners = [
+          {
+            logo:
+              "https://homologacao.gestaodetrafegonext.com.br/maratona-tech/public/images/maratona-tech/marcas/gov/parana.png",
+            alt: "accenture",
+          },
+          {
+            logo:
+              "https://homologacao.gestaodetrafegonext.com.br/maratona-tech/public/images/maratona-tech/marcas/gov/rio-grande-sul.png",
+            alt: "accenture",
+          },
+          {
+            logo:
+              "https://homologacao.gestaodetrafegonext.com.br/maratona-tech/public/images/maratona-tech/marcas/gov/salvador.png",
+            alt: "accenture",
+          },
+          {
+            logo:
+              "https://homologacao.gestaodetrafegonext.com.br/maratona-tech/public/images/maratona-tech/marcas/gov/salvador.png",
+            alt: "accenture",
+          },
+          {
+            logo:
+              "https://homologacao.gestaodetrafegonext.com.br/maratona-tech/public/images/maratona-tech/marcas/gov/santa-catarina.png",
+            alt: "Mercado Livre",
+          },
+          {
+            logo:
+              "https://homologacao.gestaodetrafegonext.com.br/maratona-tech/public/images/maratona-tech/marcas/gov/pernambuco.png",
+            alt: "accenture",
+          },
+          {
+            logo:
+              "https://homologacao.gestaodetrafegonext.com.br/maratona-tech/public/images/maratona-tech/marcas/gov/fortaleza.png",
+            alt: "accenture",
+          },
+          {
+            logo:
+              "https://homologacao.gestaodetrafegonext.com.br/maratona-tech/public/images/maratona-tech/marcas/gov/tocantins.png",
+            alt: "accenture",
+          },
+    ],
+  }: Props,
+) {
+  return (
+    <div class="lg:w-8/12 mx-auto">
+      <div class="pt-10 pb-20 px-8 lg:px-0">
+        <h3 class="text-2xl text-center font-sora font-bold text-purple-800 mb-5">
+          {title}
+        </h3>
+        <ul class="my-10 flex flex-wrap justify-center items-center gap-8">
+        {partners?.map((partner) => (
+              <li class="w-[150px] p-3">
+                <img
+                  src={partner.logo}
+                  alt={partner.alt}
+                />
+              </li>
+        ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
