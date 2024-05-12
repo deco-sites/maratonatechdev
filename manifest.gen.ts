75648771a1262a7b3f2b6b61663a71046e6be59b
jovenan <jovenan2001@gmail.com>
2024-05-11 23:11:04 -0300

    feat: add schedule section

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -10,17 +10,18 @@ import * as $$$$$$2 from "./sections/BlogPosts.tsx";
 import * as $$$$$$3 from "./sections/Faq.tsx";
 import * as $$$$$$4 from "./sections/Footer.tsx";
 import * as $$$$$$5 from "./sections/Header.tsx";
-import * as $$$$$$14 from "./sections/hero-1.tsx";
-import * as $$$$$$15 from "./sections/hero-2.tsx";
+import * as $$$$$$15 from "./sections/hero-1.tsx";
+import * as $$$$$$16 from "./sections/hero-2.tsx";
 import * as $$$$$$6 from "./sections/Hero.tsx";
-import * as $$$$$$16 from "./sections/hero2.tsx";
+import * as $$$$$$17 from "./sections/hero2.tsx";
 import * as $$$$$$7 from "./sections/ImageWithParagraph.tsx";
 import * as $$$$$$8 from "./sections/ImpactAndTestimonials.tsx";
 import * as $$$$$$9 from "./sections/Logos.tsx";
 import * as $$$$$$10 from "./sections/MediaAndAwards.tsx";
-import * as $$$$$$11 from "./sections/Testimonials.tsx";
-import * as $$$$$$12 from "./sections/Theme/Theme.tsx";
-import * as $$$$$$13 from "./sections/WhoCanParticipate.tsx";
+import * as $$$$$$11 from "./sections/Schedule.tsx";
+import * as $$$$$$12 from "./sections/Testimonials.tsx";
+import * as $$$$$$13 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$14 from "./sections/WhoCanParticipate.tsx";
 
 const manifest = {
   "sections": {
@@ -30,17 +31,18 @@ const manifest = {
     "site/sections/Faq.tsx": $$$$$$3,
     "site/sections/Footer.tsx": $$$$$$4,
     "site/sections/Header.tsx": $$$$$$5,
-    "site/sections/hero-1.tsx": $$$$$$14,
-    "site/sections/hero-2.tsx": $$$$$$15,
+    "site/sections/hero-1.tsx": $$$$$$15,
+    "site/sections/hero-2.tsx": $$$$$$16,
     "site/sections/Hero.tsx": $$$$$$6,
-    "site/sections/hero2.tsx": $$$$$$16,
+    "site/sections/hero2.tsx": $$$$$$17,
     "site/sections/ImageWithParagraph.tsx": $$$$$$7,
     "site/sections/ImpactAndTestimonials.tsx": $$$$$$8,
     "site/sections/Logos.tsx": $$$$$$9,
     "site/sections/MediaAndAwards.tsx": $$$$$$10,
-    "site/sections/Testimonials.tsx": $$$$$$11,
-    "site/sections/Theme/Theme.tsx": $$$$$$12,
-    "site/sections/WhoCanParticipate.tsx": $$$$$$13,
+    "site/sections/Schedule.tsx": $$$$$$11,
+    "site/sections/Testimonials.tsx": $$$$$$12,
+    "site/sections/Theme/Theme.tsx": $$$$$$13,
+    "site/sections/WhoCanParticipate.tsx": $$$$$$14,
   },
   "apps": {
     "site/apps/decohub.ts": $$$$$$$$$$$0,
---
+++ sections/Schedule.tsx
@@ -0,0 +1,106 @@
+import type { ImageWidget } from "apps/admin/widgets.ts";
+import Image from "apps/website/components/Image.tsx";
+
+export interface Card {
+  /**
+   * @format rich-text
+   * @default Click here to tweak this text however you want.
+  */
+  title?: string;
+  /**
+   * @format rich-text
+   * @default Click here to tweak this text however you want.
+   */
+  description?: string;
+  /** @format color-input */
+  color?: string;
+}
+
+export interface Props {
+  desktopBackgroundImage?: ImageWidget;
+  mobileBackgroundImage?: ImageWidget;
+  /**
+   * @format rich-text
+   * @default Click here to tweak this text however you want.
+  */
+  title?: string;
+  cards?: Card[];
+}
+
+export default function Schedule({
+  desktopBackgroundImage = 'https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/9084/e6f55904-0276-4040-8f57-c3b635e10ae9',
+  mobileBackgroundImage = 'https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/9084/14fae646-3035-4432-a351-6024ac2f9faa',
+  title = 'Quem pode se inscrever?',
+  cards = [
+    {
+      title: 'Professores',
+      description: 'de Escolas Públicas e Privadas',
+      color: '#ec4899'
+    },
+    {
+      title: 'Nível 01',
+      description: '6º e 7º Ano',
+      color: '#eab308'
+    },
+    {
+      title: 'Nível 02',
+      description: '8º e 9º Ano',
+      color: '#3b82f6'
+    },
+    {
+      title: 'Nível 03',
+      description: 'Ensino Médio',
+      color: '#a855f7'
+    },
+  ]
+}: Props) {
+  return (
+    <>
+      <style dangerouslySetInnerHTML={{
+        __html: `
+          .quem-pode{
+            background-color: rgb(2,6,23);
+            background: linear-gradient(90deg, rgba(2,6,23,0) 0%, rgba(2,6,23,1) 60%),url('${desktopBackgroundImage}');
+            background-size: cover;
+            background-position: top center;
+          }
+          /* MOBILE */
+          @media only screen and (max-width: 600px) {
+            .quem-pode{
+              background-color: rgb(1,17,43)!important;
+              background: linear-gradient(0deg, rgba(1,17,43,0) 100%, rgba(1,17,43,1) 0%),url('${mobileBackgroundImage}');
+              background-size: contain;
+              background-position: bottom center;
+              background-repeat: no-repeat;
+            } 
+          }  
+        `,
+      }} />
+      <div 
+        class="quem-pode bg-slate-950 py-20 space-y-10 px-10 lg:px-0 text-white -mt-10 min-h-[1200px] lg:min-h-[450px] flex lg:items-center justify-end -skew-y-2"
+      >
+        <div class="lg:w-10/12 mx-auto flex justify-end skew-y-2">
+          <div class="lg:w-6/12">
+            <h1 class="text-3xl font-sora font-bold text-center" dangerouslySetInnerHTML={{
+              __html: title
+            }} />
+            <div class="grid lg:grid-cols-2 gap-3 my-8">
+              {
+                cards.map(card => (
+                  <div style={{
+                    borderColor: card.color
+                  }} class="text-center lg:text-left rounded-2xl p-8 border">
+                    <h1 style={{
+                      color: card.color
+                    }} class="text-2xl font-bold font-sora">{card.title}</h1>
+                    <p>{card.description}</p>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
