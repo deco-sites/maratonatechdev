cb20737203fdb16cfffe15105d04bf4dc554d054
Martin Adam <martin.viana99@gmail.com>
2024-05-12 03:29:18 -0300

    h3 margin fix

--- sections/Sponsors.tsx
+++ sections/Sponsors.tsx
@@ -117,7 +117,7 @@ export default function Section(
   return (
     <div class="lg:w-8/12 mx-auto">
       <div class="pt-10 pb-20 px-8 lg:px-0">
-        <h3 class="text-2xl text-center font-sora font-bold text-purple-800 mb-5">
+        <h3 class="text-2xl text-center font-sora font-bold text-purple-800 mb-10">
           {title}
         </h3>
         <img
